4bb4caca72975b16395a63e216ebe1506f143a6c
Billy Endres <43163106+billyendres@users.noreply.github.com>
2021-03-02 15:12:35 +1000

    Update api.js

--- src/binanceAPI/api.js
+++ src/binanceAPI/api.js
@@ -1,8 +1,7 @@
 import Binance from "node-binance-api";
 
 const binance = new Binance().options({
-  APIKEY: "2pirB2JwcBRL4dtPVhffamXwgQtoWpZxRZA1ifpQ3kNV1HfrFcIdxN5JVclcCqZV",
-  APISECRET: "OyokS6Bid5QGkRj2WuzhoW8ocUZMYzswvZjtEvYc7KrtXekohVnNCOTWujHL2gy2",
+//////////////////////////////////////
   useServerTime: true,
 });
 
